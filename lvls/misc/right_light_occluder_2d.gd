a7e33e0599c99dd95b19a8fcb394e6ca9cc7037f
corndev <corndev@mail.ru>
2024-12-17 05:21:03 +0300

    light fix
    it was light but not on the performance i feel but actually its good

---
+++ lvls/misc/right_light_occluder_2d.gd
@@ -0,0 +1,35 @@
+extends LightOccluder2D
+
+
+
+@export var left = false
+@onready var player = get_tree().get_nodes_in_group("player")[0]
+@onready var da_x = global_position.x
+var active = true
+var lmask
+
+
+func _ready() -> void:
+	lmask = occluder_light_mask
+
+func _physics_process(delta: float) -> void:
+	if !left:
+		if active and player.global_position.x > da_x:
+			switch()
+		elif !active and player.global_position.x < da_x:
+			switch()
+	else:
+		if active and player.global_position.x < da_x:
+			switch()
+		elif !active and player.global_position.x > da_x:
+			switch()
+
+func switch() -> void:
+	if active:
+		#print(get_occluder_light_mask())
+		set_occluder_light_mask(0)
+		active = !active
+	else:
+		#print(get_occluder_light_mask())
+		set_occluder_light_mask(2)
+		active = !active
