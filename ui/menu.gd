21813a33e8fdbb24fb5e0214e028ce7d2c53f03e
corndev <corndev@mail.ru>
2024-12-19 09:35:02 +0300

    music done

--- ui/menu.gd
+++ ui/menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 
 func _on_start_button_pressed() -> void:
+	get_tree().paused = false
 	OstManager.switch_track("Shop")
 	get_tree().call_deferred("change_scene_to_file","res://lvls/world_1_shop.tscn")
 
