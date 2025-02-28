07a1b4bbca9cdf22755820fb4864c6bf9fef3301
corndev <corndev@mail.ru>
2025-02-28 20:59:04 +0300

    recoil fix

--- obj/bodies/cursor.gd
+++ obj/bodies/cursor.gd
@@ -8,7 +8,8 @@ func _physics_process(delta):
 	global_position = global_position.lerp(mouse_pos, delta * FOLLOW_SPEED)
 
 func apply_recoil(recoil_vector):
-	
-	var new_len = position.length() + recoil_vector.x
-	print(recoil_vector.y)
-	position = (position.normalized() * new_len).rotated(deg_to_rad(recoil_vector.y * 0.1))
+	var los_vec = global_position - get_parent().global_position
+	print(los_vec)
+	var new_len = los_vec.length() + recoil_vector.x
+	var change_vec = los_vec - (los_vec.normalized() * new_len).rotated(deg_to_rad(recoil_vector.y * 0))
+	global_position -= change_vec
