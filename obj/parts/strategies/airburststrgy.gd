16ba0044ef9ec02b621999a17a440fdb74f0df36
corndev <corndev@mail.ru>
2025-03-10 10:19:27 +0300

    airburst fix vol.2

--- obj/parts/strategies/airburststrgy.gd
+++ obj/parts/strategies/airburststrgy.gd
@@ -5,6 +5,6 @@ var base_range
 func apply_strategy(bullet : Node2D,gun : Node2D):
 	if !base_range: base_range = gun.stats.range
 	var distance = (gun.get_global_mouse_position() - gun.get_point_of_fire()).length()
-	var velocity = (Vector2.RIGHT * (bullet.speed + gun.add_spd)).rotated(bullet.global_rotation)
+	var velocity = (Vector2.RIGHT * (bullet.speed + gun.stats.add_spd)).rotated(bullet.global_rotation)
 	gun.stats.range = distance / velocity.length()
 	if gun.stats.range > base_range: gun.stats.range = base_range
